f914ee604a7503071ddbbfc584c6faac306b3423
Cherpakov-Philipp-MSP <cherpakov@msp.ua>
2019-04-17 19:43:31 +0300

    React - initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Header } from './components/header/Header';
+import { Main } from './components/main/Main';
+import { Footer } from './components/footer/Footer';
+
+import './styles/general.scss';
+
+import { createDeflate } from 'zlib';
+
+
+const rootComp = (
+    <>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </>
+);
+
+ReactDOM.render(rootComp, document.getElementById('app'));
+
+
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navigation } from '../navigation/Navigation';
+
+import './header.scss';
+
+export const Header = () => (
+  <div className="header">
+    <Navigation/>
+  </div>
+);
