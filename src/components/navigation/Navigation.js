5bdd9bfecffa8b9ffa162bc077600b963e390095
Philipp-desctop <cherpakov.filipp@gmail.com>
2019-05-16 23:16:56 +0300

    ht20 - new components (NotFound, Loader, Pages), routing Navigation comp.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 const links = [
   { label: 'Home', path: '', icon: 'home', auth: false },
-  { label: 'Home', path: '', icon: 'home', auth: true },
+  { label: 'Home-auth', path: '', icon: 'home', auth: true },
   { label: 'Shop', path: 'categories', icon: 'list-alt', auth: false },
   { label: 'Categories', path: 'categories', icon: 'list-alt', auth: true },
   { label: 'Products', path: 'products', icon: 'shopping-bag', auth: true },
@@ -12,9 +12,58 @@ const links = [
 ];
 
 export const Navigation = ({ user, onLogout }) => {
+  const mainNavLinks = user ?
+    links.filter(item => {
+      if (item.auth === true || item.auth === undefined) {
+        return (
+          <li key={item.label}>
+            <NavLink to={`/${item.path}`}>{item.label}</NavLink>
+          </li>
+        )
+      }
+    })
+  :
+    links.filter(item => {
+      if (item.auth === false || item.auth === undefined) {
+        return (
+          <li key={item.label}>
+            <NavLink to={`/${item.path}`}>{item.label}</NavLink>
+          </li>
+        )
+      }
+    })
+
+  const userNavLinks = user ?
+      <>
+        <span>{user}</span>
+        <button onClick={onLogout}>Logout</button>
+      </>
+    :
+      <>
+        <Link to="/login">Log in </Link>
+        <span>/</span>
+        <Link to="/registration"> Register</Link>
+      </>
 
   return (
     <div className="navigation">
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <Link to="/"><img src="/images/logo.png" alt="logo"/></Link>
+          </li>
+          {
+            mainNavLinks.map(item => (
+              <li key={item.label}>
+                <NavLink to={`/${item.path}`}>{item.label}</NavLink>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+      <nav className="user-nav">
+        {userNavLinks}
+      </nav>
     </div>
   )
 };
