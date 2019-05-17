55298114b758044882551961b2223cbcd63a02e9
Cherpakov-Philipp-MSP <cherpakov@msp.ua>
2019-05-17 15:09:18 +0300

    ht20 - resolved error with authorisation

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import { Greeting } from './greeting';
 import { NotFound } from './notFound';
