83f5d66ce1942ceb5cefc4beafd53d92610e3b49
Cherpakov-Philipp-MSP <cherpakov@msp.ua>
2019-06-10 17:49:49 +0300

    ht28 - Category page

--- src/pages/categories/Category.js
+++ src/pages/categories/Category.js
@@ -1,33 +1,80 @@
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { FaTrashAlt } from 'react-icons/fa';
 
+import { ListWithControls } from '../../components/listWithControls';
+import { ListWithFilter } from '../../components/listWithFilter';
+import { Loader } from '../../components/loader';
 import { setCategoryAsync } from '../../store/categories';
+import { setProductsAsync, updateProductAsync } from '../../store/products';
 
 const { useEffect } = React;
 
-const CategoryComponent = ({ dispatch, match, category }) => {
+const CategoryComponent = ({ dispatch, match, history, category, products }) => {
   useEffect(() => {
     dispatch(setCategoryAsync(match.params.id));
+    dispatch(setProductsAsync());
   }, []);
 
+  const onEdit = (id, title) => {
+    const product = category.products.find(item => item.id === id);
+
+    product.title = title;
+
+    dispatch(updateProductAsync([id, category]));
+  }
+
+  const onTitleClick = (id) => {
+    history.push(`/products/${id}`);
+  }
+
+  const changePublishedStatus = (id, published) => {
+    const product = category.products.find(item => item.id === id);
+    let poductIndex;
+
+    category.products.forEach((item, index) => {
+      if (item.id === id) poductIndex = index;
+    })
+
+    published ? category.products.push(product) : category.products.splice(poductIndex, 1);
+
+    dispatch(updateProductAsync([id, category]));
+  }
+
+  const filterPublished = () => {
+    return category.products;
+  }
+
+  const filterUnpublished = () => {
+    return products.filter(item => {
+      return !category.products.includes(item);
+    })
+  }
+
   return (
+    !category || !products ? <Loader /> :
     <>
-      {category && <h1>Category {category.title}</h1>}
-      <ul className="category_products">
-        {category && category.products && category.products.map(product => (
-          <li key={product.id}>
-            <Link to={`/products/${product.id}`}>
-              {product.title}
-            </Link>
-            <FaTrashAlt className="remove" />
-          </li>
-        ))}
-      </ul>
+      <h1>Category {category.title}</h1>
+      <div className="products">
+        <div className="published">
+          <h2>Published products</h2>
+          <ListWithControls
+            items={filterPublished()}
+            onEdit={onEdit}
+            onDelete={id => changePublishedStatus(id, false)}
+            onTitleClick={onTitleClick}
+          />
+        </div>
+        <div className="unpublished">
+          <h2>Products</h2>
+          <ListWithFilter
+            items={filterUnpublished()}
+            onSelect={id => changePublishedStatus(id, true)}
+          />
+        </div>
+      </div>
     </>
-  );
+  )
 };
 
-const mapStateToProps = state => ({ category: state.category });
+const mapStateToProps = state => ({ category: state.category, products: state.products });
 
 export const Category = connect(mapStateToProps)(CategoryComponent);
